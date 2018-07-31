29140f5d4580254c087d97717895b635e4557e1e
Matt Merz <mattmerz98@gmail.com>
2018-07-31 13:41:37 -0500

    First commit of stepmasterdark, stepmasterflat Small changes to stepmasterbias, auto config

--- source/stonesteps/stepmasterdark.py
+++ source/stonesteps/stepmasterdark.py
@@ -7,6 +7,7 @@
 
     @author: Matt Merz
 """
+
 import os # os library
 import sys # sys library
 import numpy # numpy library
@@ -17,9 +18,10 @@ import matplotlib.pyplot as plt
 from astropy.io import fits #package to recognize FITS files
 from drp.stepmiparent import StepMIParent
 from drp.datafits import DataFits
+from drp.steploadaux import StepLoadAux
 
-class StepMasterBias(StepMIParent):
-    """ Stone Edge Pipeline Step Master Bias Object
+class StepMasterDark(StepLoadAux, StepMIParent):
+    """ Stone Edge Pipeline Step Master Dark Object
         The object is callable. It requires a valid configuration input
         (file or object) when it runs.
     """
@@ -42,12 +44,12 @@ class StepMasterBias(StepMIParent):
         """
         ### Set Names
         # Name of the pipeline reduction step
-        self.name='masterbias'
+        self.name='masterdark'
         # Shortcut for pipeline reduction step and identifier for
         # saved file names.
-        self.procname = 'mbias'
+        self.procname = 'mdark'
         # Set Logger for this pipe step
-        self.log = logging.getLogger('pipe.step.%s' % self.name)
+        self.log = logging.getLogger('stoneedge.pipe.step.%s' % self.name)
         ### Set Parameter list
         # Clear Parameter list
         self.paramlist = []
@@ -56,11 +58,19 @@ class StepMasterBias(StepMIParent):
                                'Specifies how the files should be combined - options are median, average, sum'])
         self.paramlist.append(['outputfolder','',
                                'Output directory location - default is the folder of the input files'])
+        # Get parameters for StepLoadAux, replace auxfile with biasfile
+        self.loadauxsetup('bias')
 
     def run(self):
         """ Runs the combining algorithm. The self.datain is run
             through the code, the result is in self.dataout.
         """
+        # Find master bias to subtract from master dark
+        biaslist = self.loadauxname('bias', multi = False)
+        if(len(biaslist) == 0):
+            self.log.error('No bias calibration frames found.')
+        self.bias = ccdproc.CCDData.read(biaslist, unit='adu', relax=True)
+        # Create empy list for filenames of loaded frames
         filelist=[]
         for fin in self.datain:
             self.log.debug("Input filename = %s" % fin.filename)
@@ -68,16 +78,17 @@ class StepMasterBias(StepMIParent):
         # Make a dummy dataout
         self.dataout = DataFits(config = self.config)
         if len(self.datain) == 0:
-            self.log.error('Bias calibration frame not found.')
-            raise RuntimeError('No bias file(s) loaded')
-        # self.log.debug('Creating master bias frame...')
-        #if there is just one, use it as biasfile or else combine all to make a master bias
+            self.log.error('Flat calibration frame not found.')
+            raise RuntimeError('No flat file(s) loaded')
+        self.log.debug('Creating master flat frame...')
+        # Create master frame: if there is just one file, turn it into master bias or else combine all to make master bias
         if (len(filelist) == 1):
-            self.bias = ccdproc.CCDData.read(filelist[0], unit='adu', relax=True)
+            self.dark = ccdproc.CCDData.read(filelist[0], unit='adu', relax=True)
         else:
-            self.bias = ccdproc.combine(filelist, method=self.getarg('combinemethod'), unit='adu', add_keyword=True)
+            self.dark = ccdproc.combine(filelist, method=self.getarg('combinemethod'), unit='adu', add_keyword=True)
+        self.dark = ccdproc.subtract_bias(self.dark, self.bias, add_keyword=False)
         self.dataout.header=self.datain[0].header
-        self.dataout.imageset(self.bias)
+        self.dataout.imageset(self.dark)
         # rename output filename
         if self.getarg('outputfolder')!='':
             self.dataout.filename = os.path.join(self.getarg('outputfolder'), os.path.split(filelist[0])[1])
