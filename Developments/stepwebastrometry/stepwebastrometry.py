89841b8d91313ca57293b47676bbc56f5c7db9b8
Josh Garza <josh@Joshs-MacBook-Air.local>
2020-08-15 22:36:00 -0500

    Added ability to upload source tables to Astrometry.net

--- Developments/stepwebastrometry/stepwebastrometry.py
+++ Developments/stepwebastrometry/stepwebastrometry.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
-""" PIPE STEP ASTROMETRY- Version 1.0.0
+""" PIPE STEP WEBASTROMETRY- Version 1.1.0
 
     This pipe step calls the external program astrometry.net to add
     WCS information to the data.
     
     @author: Josh / Prechelt / Berthoud
 """
-# TODO more descriptive for scale_lower and scale_upper
+# TODO get image width/height from NAXIS1/2, more descriptive for scale_lower and scale_upper
 
 import logging # logging object library
-import tempfile # temporary file library
-import os # library for operating system calls
 from astropy import wcs # to get WCS coordinates
 from astropy.io import fits
 from astropy.coordinates import Angle
+from astropy.table import Table
 import astropy.units as u
 from darepype.drp import DataFits
 from darepype.drp import StepParent
 from astroquery.astrometry_net import AstrometryNet
+import pandas as pd
+import numpy as np
 
 class StepWebAstrometry(StepParent):
     """ HAWC Pipeline Step Parent Object
@@ -62,42 +63,73 @@ class StepWebAstrometry(StepParent):
         self.paramlist.append(['scale_upper', 2.,
                                'upper scale'])
         self.paramlist.append(['scale_units', 'arcsecperpix',
-                               'Units of degrees/pixel'])
+                               'Image plate scale units'])
         self.paramlist.append(['api_key', 'XXXXXXXX',
                                'API key used for interfacing with Astrometry.net'])
         # confirm end of setup
         self.log.debug('Setup: done')
 
     def astrometrymaster(self):
-        '''
-        Runs Astrometry.net on the inputted image
+        ''' Runs Astrometry.net on the inputted image/sources table
         '''
         
         ast = AstrometryNet()
         ast.api_key = self.getarg('api_key')
 
+        # Check whether the file contains image or table data
+        upload = True
+        for hdu in fits.open(self.datain.filename):
+            try:
+                # FITS files use big endian, so it must be converted to little endian before it can be used by numpy/pandas
+                tbl = pd.DataFrame(np.array(hdu.data).byteswap(inplace=True).newbyteorder())
+                tbl.columns = ['X_IMAGE', 'Y_IMAGE', 'a', 'b', 'c', 'd', 'FLUX']
+                tbl = tbl.sort_values(by='FLUX', axis=0, ascending=False)
+            except:
+                pass
+            else:
+                upload = False
+
+        if upload:
+            self.log.debug('File to reduce has not been source extracted, uploading image to Astrometry.net')
+        else:
+            self.log.debug('File to reduce has been source extracted, uploading source table to Astrometry.net')
+
         self.log.debug("Now running ast.solve, get comfy this'll take a while")
 
+        # If the header contains RA and Dec, use them to solve
         try:
             ra = Angle(self.datain.getheadval('RA'), unit=u.hour).degree
             dec = Angle(self.datain.getheadval('DEC'), unit=u.deg).degree
         except:
-            self.wcs_out = ast.solve_from_image(self.datain.filename, force_image_upload = True, solve_timeout = self.getarg('timeout'), 
-                                                scale_lower = self.getarg('scale_lower'), scale_upper = self.getarg('scale_upper'), 
-                                                scale_units = self.getarg('scale_units'))
+            if upload:
+                self.log.debug('Solving from image without RA/Dec')
+                self.wcs_out = ast.solve_from_image(self.datain.filename, force_image_upload = True, solve_timeout = self.getarg('timeout'), 
+                                                    scale_lower = self.getarg('scale_lower'), scale_upper = self.getarg('scale_upper'), 
+                                                    scale_units = self.getarg('scale_units'))
+            else:
+                self.log.debug('Solving from source list without RA/Dec')
+                self.wcs_out = ast.solve_from_source_list(x=tbl['X_IMAGE'], y=tbl['Y_IMAGE'], image_width=1024., image_height=1024., 
+                                                          solve_timeout=self.getarg('timeout'))
         else:
-            self.wcs_out = ast.solve_from_image(self.datain.filename, force_image_upload = True, ra_key = 'RA', dec_key = 'DEC', 
-                                                solve_timeout = self.getarg('timeout'), radius = self.getarg('radius'), 
-                                                scale_lower = self.getarg('scale_lower'), scale_upper = self.getarg('scale_upper'), 
-                                                scale_units = self.getarg('scale_units'))
+            if upload:
+                self.log.debug('Solving from image with RA/Dec')
+                self.wcs_out = ast.solve_from_image(self.datain.filename, force_image_upload = True, ra_key = 'RA', dec_key = 'DEC', 
+                                                    solve_timeout = self.getarg('timeout'), radius = self.getarg('radius'), 
+                                                    scale_lower = self.getarg('scale_lower'), scale_upper = self.getarg('scale_upper'), 
+                                                    scale_units = self.getarg('scale_units'))
+            else:
+                self.log.debug('Solving from source list with RA/Dec')
+                self.wcs_out = ast.solve_from_source_list(x=tbl['X_IMAGE'], y=tbl['Y_IMAGE'], image_width=1024., image_height=1024., 
+                                                          solve_timeout=self.getarg('timeout'), radius=self.getarg('radius'), center_ra=ra, 
+                                                          center_dec=dec)
 
+        # Check if the solution completed, though it should error if it times out anyway
         try:
-            #Check if WCS exists
             self.wcs_out
         except:
-            self.log.error("Unable to perform web astrometry.")
+            self.log.error('Failed to find WCS solution')
         else:
-            self.log.debug('Web astrometry successful')
+            self.log.debug('Web astrometry completed successfully')
 
 
     def run(self):
@@ -106,23 +138,28 @@ class StepWebAstrometry(StepParent):
         """
         
         self.dataout = DataFits(config=self.config)
+        self.dataout = self.datain.copy()
         self.astrometrymaster()
 
-        ## Post-processing
-        self.dataout = self.datain.copy()
+        # Update RA/Dec from astrometry
         self.dataout.header.update(self.wcs_out)
-        # Add RA from astrometry
-        w = wcs.WCS(self.dataout.header)
-        n1 = float( self.dataout.header['NAXIS1']/2 )
-        n2 = float( self.dataout.header['NAXIS2']/2 )
-        ra, dec = w.all_pix2world(n1, n2, 1)
-        # No update because update may affect accuracy of WCS solution
-        # self.dataout.header['CRPIX1']=n1
-        # self.dataout.header['CRPIX2']=n2
-        # self.dataout.header['CRVAL1']=float(ra)
-        # self.dataout.header['CRVAL2']=float(dec)
-        self.dataout.header['RA'] = Angle(ra,  u.deg).to_string(unit=u.hour, sep=':')
-        self.dataout.header['Dec'] = Angle(dec, u.deg).to_string(sep=':')
+        try:
+            w = wcs.WCS(self.dataout.header)
+            n1 = float( self.dataout.header['NAXIS1']/2 )
+            n2 = float( self.dataout.header['NAXIS2']/2 )
+            ra, dec = w.all_pix2world(n1, n2, 1)
+            # No update because update may affect accuracy of WCS solution
+            # self.dataout.header['CRPIX1']=n1
+            # self.dataout.header['CRPIX2']=n2
+            # self.dataout.header['CRVAL1']=float(ra)
+            # self.dataout.header['CRVAL2']=float(dec)
+            self.dataout.header['RA'] = Angle(ra,  u.deg).to_string(unit=u.hour, sep=':')
+            self.dataout.header['Dec'] = Angle(dec, u.deg).to_string(sep=':')
+        except:
+            self.log.error('Run: Could not update RA/Dec from Astrometry')
+        else:
+            self.log.debug('Run: Updated RA/Dec from Astrometry')
+
         self.log.debug('Run: Done')
     
 if __name__ == '__main__':
@@ -138,6 +175,7 @@ if __name__ == '__main__':
     StepWebAstrometry().execute()
 
 """ === History ===
+2020-08-13  -Added ability to upload source tables to Astrometry
 2020-08-10  -Removed overwriting of RAW files
 2020-08-07  -Removed unused code from previous version, combined webastrometry
              and astrometrymaster, API key can now be specified in config
