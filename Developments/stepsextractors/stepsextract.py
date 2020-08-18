f8f3f5ad07f65b394d04283878a2ce7b7e50c876
Daniel S <dsharkey@uchicago.edu>
2020-08-18 16:53:44 -0400

    removed unnecessary code

--- Developments/stepsextractors/stepsextract.py
+++ Developments/stepsextractors/stepsextract.py
@@ -154,81 +154,6 @@ class StepSextract(StepParent):
         if self.getarg('delete_cat'):
             os.remove(catfilename)
 
-        ### Query and extract data from Guide Star Catalog
-        # Get RA / Dec
-        '''
-        ra_center =  self.datain.getheadval('RA' ).split(':')
-        dec_center = self.datain.getheadval('DEC').split(':')
-        ra_cent =  ' '.join([str(s) for s in ra_center])
-        dec_cent = ' '.join([str(s) for s in dec_center])
-        center_coordinates = SkyCoord(ra_cent + ' ' + dec_cent, unit=(u.hourangle, u.deg) )
-        self.log.debug('Using RA/Dec = %s / %s' % (center_coordinates.ra, center_coordinates.dec) )
-        # Querry guide star catalog2 with center coordinates
-        gsc2_query = 'http://gsss.stsci.edu/webservices/vo/CatalogSearch.aspx?'
-        gsc2_query += 'RA='+str(center_coordinates.ra.value)
-        gsc2_query += '&DEC='+str(center_coordinates.dec.value)
-        gsc2_query += '&DSN=+&FORMAT=CSV&CAT=GSC241&SR=0.5&'
-        self.log.debug('Running URL = %s' % gsc2_query)
-        gsc2_result = requests.get(gsc2_query)
-        # Get data from result
-        filter_map = self.getarg('filtermap').split('|')
-        filter_name = filter_tel = self.datain.getheadval('FILTER')
-        for fil in filter_map:
-            entry = fil.split('=')
-            if entry[0] == filter_tel:
-                try:
-                    filter_name = entry[1]
-                except:
-                    self.log.error("Badly formatted filter mapping. No '=' after %s"
-                                   % filter_tel)
-        query_table = astropy.io.ascii.read(gsc2_result.text)
-        table_filter = 'SDSS'+filter_name+'Mag'
-        table_filter_err = 'SDSS'+filter_name+'MagErr'
-        GSC_RA = query_table['ra'][(query_table[table_filter]<22) & (query_table[table_filter]>0)]
-        GSC_DEC = query_table['dec'][(query_table[table_filter]<22) & (query_table[table_filter]>0)]
-        GSC_Mag = query_table[table_filter][(query_table[table_filter]<22) & (query_table[table_filter]>0)]
-        GSC_MagErr = query_table[table_filter_err][(query_table[table_filter]<22) & (query_table[table_filter]>0)]
-        self.log.debug('Received %d entries from Guide Star Catalog' % len(GSC_RA))
-        ### Mach Guide Star Catalog data with data from Source Extractor
-        # Do the matching
-        seo_radec = SkyCoord(ra=seo_catalog['ALPHA_J2000'], dec=seo_catalog['DELTA_J2000'])
-        GSC_radec = SkyCoord(ra=GSC_RA*u.deg, dec=GSC_DEC*u.deg)
-        idx, d2d, d3d = GSC_radec.match_to_catalog_sky(seo_radec[seo_SN])
-        # only select objects less than 0.025 away in distance, get distance value
-        dist_value = 1*0.76*binning/3600. #Maximum distance is 1 pixel
-        mask = d2d.value<dist_value
-        if(np.sum(mask) < 2):
-            self.log.warn('Only %d sources match between image and guide star catalog, fit may not work' %
-                          np.sum(mask) )
-        self.log.debug('Distance_Value = %f, Min(distances) = %f, Mask length = %d' %
-                       ( dist_value, np.min(d2d.value), np.sum(mask) ) )
-        ### Calculate the fit correction between the guide star and the extracted values
-        # Make lambda function to be minimized
-        # The fit finds m_ml and b_ml where
-        #     seo_Mag = b_ml + m_ml * GSC_Mag
-        nll = lambda *args: -residual(*args)
-        # Get errors
-        eps_data = np.sqrt(GSC_MagErr**2+seo_MagErr[seo_SN][idx]**2)
-        # Make estimate for intercept to give as initial guess
-        b_ml0 = np.median(seo_Mag[seo_SN][idx][mask]-GSC_Mag[mask])
-        self.log.debug('Offset guess is %f mag' % b_ml0)
-        # Calculate distance from that guess and get StdDev of distances
-        guessdistances = np.abs( b_ml0 - ( seo_Mag[seo_SN][idx] - GSC_Mag ) )
-        guessdistmed = np.median(guessdistances[mask])
-        # Update mask to ignore values with large STDEVS
-        mask = np.logical_and( d2d.value < dist_value, guessdistances < 5 * guessdistmed )
-        self.log.debug('Median of distance to guess = %f, Mask length = %d' %
-                       ( guessdistmed, np.sum(mask) ) )
-        # Solve linear equation
-        result = scipy.optimize.minimize(nll, [1, b_ml0],
-                                         args=(GSC_Mag[mask],
-                                               seo_Mag[seo_SN][idx][mask],
-                                               eps_data[mask]))
-        m_ml, b_ml = result["x"]
-        self.log.info('Fitted offset is %f mag, fitted slope is %f' % (b_ml, m_ml) )
-        b_ml_corr = b_ml + (m_ml-1) * np.median(GSC_Mag[mask])
-        self.log.info('Corrected offset is %f mag' % b_ml_corr)
-        '''
         ### Make table with all data from source extractor
         # Collect data columns
         cols = []
@@ -253,57 +178,15 @@ class StepSextract(StepParent):
         ### Make output data
         # Copy data from datain
         self.dataout = self.datain
-        '''
-       
-        # Scale the image using calculated b_ml_corr
-        image_background = fits.open(bkgdfilename)[0].data
-        #bzero = np.nanpercentile(self.dataout.image,self.getarg('zeropercent'))
-        bzero = image_background
-        #-- Alternative bzero idea:
-        #-mask = image_array < np.percentile(image,90)
-        #-bzero = np.median(image_array[mask])
-        #bscale = 3631. * 10 ** (b_ml_corr/2.5)
-        #self.dataout.image = bscale * (self.dataout.image - bzero)
-        '''
+
         # Add sources and fitdata table
         self.dataout.tableset(sources_table.data,'Sources',sources_table.header)
         
-        #self.dataout.tableset(fitdata_table.data,'Fit Data',fitdata_table.header)
         
         # Remove background file if it's not needed
         if not self.getarg('savebackground'):
             os.remove(bkgdfilename)
-        '''
         
-        ### If requested make a plot of the fit and save as png
-        if self.getarg('fitplot'):
-            # Set up plot
-            plt.figure(figsize=(10,7))
-            # Plot 5sigma error range
-            gmin = min(GSC_Mag[mask])
-            gmax = max(GSC_Mag[mask])
-            plt.fill([gmin,gmin,gmax,gmax],[gmin+b_ml0-guessdistmed, gmin+b_ml0+guessdistmed,
-                                            gmax+b_ml0+guessdistmed, gmax+b_ml0-guessdistmed],'c')
-            # Plot fits
-            plt.plot(GSC_Mag[mask],m_ml*GSC_Mag[mask]+b_ml)
-            plt.plot(GSC_Mag[mask],GSC_Mag[mask]+b_ml0)
-            # Plot the datapoints
-            plt.errorbar(GSC_Mag[d2d.value<dist_value],seo_Mag[seo_SN][idx][d2d.value<dist_value],
-                         yerr=np.sqrt(eps_data[d2d.value<dist_value]**2),fmt='o',linestyle='none')
-            plt.errorbar(GSC_Mag[mask],seo_Mag[seo_SN][idx][mask],
-                         yerr=np.sqrt(eps_data[mask]**2),fmt='o',linestyle='none')
-            #plt.plot(GSC_Mag[d2d.value<dist_value],m_ml*GSC_Mag[d2d.value<dist_value]+zeropoint_fit[1])
-            plt.legend(['LM-fit','Fit-Guess','GuessDistMed Range','d<distval Data','Good Data'])
-            plt.ylabel('Source extrator magnitude')
-            plt.xlabel('Star catalog magnitude')
-            plt.title('Calibration Fit for file\n' + os.path.split(self.dataout.filename)[1])
-            # Plot the fit
-            # Axis and labels
-            # Save the image
-            pngname = self.dataout.filenamebegin + 'FCALplot.png'
-            plt.savefig(pngname)
-            self.log.debug('Saved fit plot under %s' % pngname)
-            '''
         ### If requested make a text file with the sources list
         if self.getarg('sourcetable'):
 
@@ -322,16 +205,7 @@ class StepSextract(StepParent):
                         format = self.getarg('sourcetableformat'))
             self.log.debug('Saved sources table under %s' % txtname)
 
-'''
-def residual(params, x, data, errors):
-    """ Fitting function for lmfit
-    """
-    m, c = params
-    model = m*x+c
-    inv_sigma2 = 1.0/(errors**2)
-    #print(m,c,-0.5*(np.sum(((data-model)**2)*inv_sigma2)))
-    return -0.5*(np.sum(((data-model)**2)*inv_sigma2))
-'''
+
 
 if __name__ == '__main__':
     """ Main function to run the pipe step from command line on a file.
