03151de139b0c3da2666deaab4bb5f26eeec00f8
pancakefries <39656573+pancakefries@users.noreply.github.com>
2021-02-11 18:29:53 -0600

    Fixed compatilibility with stepsrcextpy.py
    - Changed default table name to "HTS"
    - Step no longer breaks after changing number of table columns
    - Added error message if step fails to get table data

--- source/stonesteps/stepastrometryweb.py
+++ source/stonesteps/stepastrometryweb.py
@@ -73,7 +73,7 @@ class StepAstrometryWeb(StepParent):
                                'Image plate scale units'])
         self.paramlist.append(['api_key', 'XXXXXXXX',
                                'API key used for interfacing with Astrometry.net'])
-        self.paramlist.append(['table_name', '',
+        self.paramlist.append(['table_name', 'HTS',
                                'Name of table that should be used when solving'])
         # confirm end of setup
         self.log.debug('Setup: done')
@@ -97,10 +97,12 @@ class StepAstrometryWeb(StepParent):
             else:
                 hdu = self.datain.tableget()
             tbl = pd.DataFrame(np.array(hdu).byteswap(inplace=True).newbyteorder())
-            tbl.columns = ['ID', 'X_IMAGE', 'Y_IMAGE', 'FLUX', 'HL_RADIUS']
+            col_names = ['ID', 'X_IMAGE', 'Y_IMAGE', 'FLUX']
+            [col_names.append('PLACEHOLDER' + str(i)) for i in range(len(tbl.columns) - 4)]
+            tbl.columns = col_names
             tbl = tbl.sort_values(by='FLUX', axis=0, ascending=False)
         except:
-            pass
+            self.log.error('ERROR: Encountered error getting table data')
         else:
             nosourcetable = False
 
