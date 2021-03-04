b139a868be88ed027ceacee17eb0607ac9f16e15
pancakefries <39656573+pancakefries@users.noreply.github.com>
2021-03-04 00:27:14 -0600

    StepAstrometryLocal no longer removes Table HDUs
    - All HDUs are now copied to self.dataout before a temporary object is used to load and transfer the updated WCS

--- source/stonesteps/stepastrometrylocal.py
+++ source/stonesteps/stepastrometrylocal.py
@@ -171,10 +171,13 @@ class StepAstrometryLocal(StepParent):
 
         ### Post processing
         # Read output file
-        self.dataout = DataFits(config=self.config)
+        self.dataout = self.datain.copy()
+        tempobj = DataFits(config=self.config)
         self.log.debug('Opening astrometry.net output file %s' % outnewname)
         try:
-            self.dataout.load(outnewname)
+            tempobj.load(outnewname)
+            self.dataout.header = tempobj.header
+            self.dataout.image = tempobj.image
             self.dataout.filename = self.datain.filename
         except Exception as error:
             self.log.error("Unable to open astrometry. output file = %s"
