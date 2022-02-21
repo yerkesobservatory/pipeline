fd156645c8919cba9db69693744838672ee588ad
berthoud <mgb11@cornell.edu>
2022-02-21 14:12:05 -0600

    Updates from stars

--- PipeExecuteAutoDay.py
+++ PipeExecuteAutoDay.py
@@ -109,7 +109,7 @@ def execute():
         #result = pipe(imagelist)
         try:
             #pass
-            result = pipe(imagelist, pipemode = 'seo_server',force=True)
+            result = pipe(imagelist, pipemode = 'seo_server_hdr',force=True)
         except Exception as e:
             log.warning("Pipeline for object = %s returned Error" % entry)
             log.warning('Found Error = %s' % repr(e))
