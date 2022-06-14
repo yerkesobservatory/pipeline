2fd04e90d4aa1f16286690bb46f0687f5384fb22
Lorenzo Orders <ordelore@gmail.com>
2022-06-14 14:25:07 -0500

    added more features as requested from the SEO team

--- utils/image_review/image_review.py
+++ utils/image_review/image_review.py
@@ -1,4 +1,10 @@
-# Code written by Lorenzo Orders in Winter and Spring Quarters of 2022.
+'''
+Title: image_review.py
+Description: This program will allow the user to review a batch set of FITS
+    images and allows them to potentially output those filenames into a piperun
+    that can be interpreted by the SEO pipeline for further processing
+Author: Lorenzo Orders
+'''
 
 import sys
 import argparse
@@ -11,37 +17,80 @@ import matplotlib.cm as cm
 from astropy.stats import mad_std
 import astropy.io.fits as fits
 import configobj
+import webbrowser
+from astropy.visualization import ZScaleInterval
 
 # Constants
 # If the image goes beyond the bounds of your screen, feel free to scale it down
 SCREEN_SIZE = (800, 800)
 
-# If the filenames extned beyond the bounds of your screen,
-# you can scale down the size of the text
+# If the filenames extend beyond the bounds of your screen, you can scale down
+# the size of the text
 FONT_SIZE = 18
 
-# This function converts a 2D array of scalar values to a 2D array of RGB values
-# min/max is borrowed from Al's imgplot function
-# scaling_func MUST be a function that takes in the image data and returns the
-# minimum and maximum values for the colormap
+# When multiple lines are rendered onscreen, at once, this determines the spacing
+# between each line.
+TEXT_PADDING = 2
+
+# these are the different colormaps the program can display. If you want to add more,
+# visit https://matplotlib.org/stable/gallery/color/colormap_reference.html
+COLORMAP_NAMES = ['gist_yarg', 'gist_gray', 'turbo', 'gist_ncar', 'Set1']
 
 
-def conv_to_cmap(image, scaling_func):
-    # vmin = med-mad
-    # vmax = med + 3*mad
+def conv_to_cmap(image, scaling_func, cmapidx):
+    """
+    conv_to_cmap will take a 2D array, a scaling function, and an index for the
+    colormap to use it will return an RGB image where the scaling function and
+    colormap are used to convert each value to an RGB color
+
+    Parameters
+    ----------
+    image : ndarray
+        The 2D array of scalar values to convert to an RGB image
+    scaling_func : function (image : ndarray) -> (vmin : float, vmax : float)
+        The function that will calculate vmin and vmax for the image
+    cmapidx : int
+        The index of the matplotlib colormap to use
+
+    Returns
+    -------
+    rgb_image : ndarray
+        A 3D array of RGB values. The first two dimensions are the same as the
+        input image.
+    """
     vmin, vmax = scaling_func(image)
-    cmap = cm.get_cmap('binary')
+    cmap = cm.get_cmap(COLORMAP_NAMES[cmapidx])
     norm = colors.Normalize(vmin=vmin, vmax=vmax, clip=True)
     mapper = cm.ScalarMappable(norm=norm, cmap=cmap)
     return mapper.to_rgba(image, bytes=True)
 
-# based on Al's notebook scaling
-
 
 def notebook_scaling(image):
+    """
+    notebook scaling will take a 2D ndarray and return the vmin and vmax that
+    should form the minimum and maximum color values. Any values beyond those
+    bounds will be clamped to the nearest bound. Each of these scaling functions
+    have the same function signature, so only this function will have a docstring.
+    If you create your own function, it is vital to match the function signature
+    exactly.
+    
+    Parameters
+    ----------
+    image : ndarray
+        The 2D array of scalar values to form the basis of the vmin/vmax calculation
+
+    Returns
+    -------
+    vmin : float
+        The minimum value that should be used to form the color map
+    vmax : float
+        The maximum value that should be used to form the color map
+    """
     med = np.nanmedian(image)
     mad = mad_std(image, ignore_nan=True)
-    return med-mad, med + 3*mad
+    vmin = med - mad
+    vmax = med + 3 * mad
+    return vmin, vmax
 
 
 def scaling_90(image):
@@ -65,29 +114,86 @@ def scaling_99(image):
 def even_scaling(image):
     med = np.nanmedian(image)
     mad = mad_std(image, ignore_nan=True)
-    return med-3*mad, med + 3*mad
+    vmin = med - 3 * mad
+    vmax = med + 3 * mad
+    return vmin, vmax
+
+
+def zscale_scaling(image):
+    zscale = ZScaleInterval()
+    vmin, vmax = zscale.get_limits(image)
+    return vmin, vmax
 
 
 # if you make your own scaling function, remember to add it to the list
 SCALING_FUNCS = [notebook_scaling, scaling_90,
-                 scaling_95, scaling_99, even_scaling]
+                 scaling_95, scaling_99, even_scaling, zscale_scaling]
 
 
-def main(filenames, startindex, displaykeys, hduindex, displayhduname):
+def render_multiline_text(text, font_renderer):
+    """
+    render_multiline_text will take a string and a font renderer and return
+    a new pygame surface where the string is displayed and none of the text
+    goes off the screen
+    
+    Parameters
+    ----------
+    text : str
+        The string to display
+    font_renderer : pygame.freetype.Font
+        The font renderer to use to render the text
+    
+    Returns
+    -------
+    text_surface : pygame.Surface
+        The surface containing the text
+    text_render_pos : tuple
+        Assuming that the text is rendered at the bottom of the screen, this
+        is the position to render text_surface at
+    """
+    words = text.split(' | ')
+    line_len = 0
+    screen_width = SCREEN_SIZE[0]
+    screen_height = SCREEN_SIZE[1]
+    # surface to render the text onto
+    text_surface = pygame.Surface(SCREEN_SIZE)
+    text_surface.fill((255, 255, 255))
+    # the position to render the text surface on the main screen
+    _, size_info = font_renderer.render(text[0])
+    text_height = size_info.height
+
+    text_render_pos = [0, screen_height - text_height - TEXT_PADDING]
+    lines = 0
+    for word in words:
+        if len(word) == 0:
+            continue
+        word_surface, word_rect = font_renderer.render(
+            word + " | ", bgcolor="white", fgcolor="black")
+        line_len += word_rect.width
+        # if rendering this word would have exceeded the length of the
+        # screen, then go to a new line
+        if line_len > screen_width:
+            text_render_pos[1] -= word_rect.height
+            line_len = word_rect.width
+            lines += 1
+        text_pen = (line_len - word_rect.width,
+                    (text_height + TEXT_PADDING) * lines)
+        text_surface.blit(word_surface, text_pen)
+    return text_surface, text_render_pos
+
+
+def main(filenames, displaykeys, hduindex, displayhduname):
     pygame.init()
     display = pygame.display.set_mode(SCREEN_SIZE)
     pygame.display.set_caption("Image Review")
-    if startindex < 0:
-        startindex = 0
-    if startindex >= len(filenames):
-        startindex = len(filenames)-1
 
-    current_image = startindex
+    current_image = 0
     pygame.freetype.init()
 
     screen_font = pygame.freetype.SysFont("Arial", size=FONT_SIZE)
 
     scaling_index = 0
+    colormap_index = 0
     saving_files = set()
 
     while True:
@@ -98,13 +204,21 @@ def main(filenames, startindex, displaykeys, hduindex, displayhduname):
         # load a specific hdu
         if hduindex < 0 or hduindex >= len(fits_hdulist):
             print("HDU index out of bounds")
-            return
+            return saving_files
+
         fits_data = fits_hdulist[hduindex].data
         fits_header = fits_hdulist[hduindex].header
+        primary_header = fits_hdulist[0].header
         fits_hdulist.close()
 
-        # draw the fits image as the background
-        fits_img = conv_to_cmap(fits_data, SCALING_FUNCS[scaling_index])
+        # check for a binary table
+        if 'XTENSION' in fits_header and fits_header['XTENSION'] == 'BINTABLE':
+            print("Binary table detected. Please use a FITS image")
+            return saving_files
+
+        # draw the fits image
+        fits_img = conv_to_cmap(
+            fits_data, SCALING_FUNCS[scaling_index], colormap_index)
         display.fill('black')
         fits_buffer = pygame.image.frombuffer(
             fits_img.tobytes(), (fits_img.shape[1], fits_img.shape[0]), 'RGBA')
@@ -124,14 +238,26 @@ def main(filenames, startindex, displaykeys, hduindex, displayhduname):
             info_row += "Not Saved | "
 
         for key in displaykeys:
-            if key in fits_header:
-                info_row += f"{displaykeys[key]}: {fits_header[key]} | "
-        file_info = screen_font.render(
-            info_row, bgcolor="white", fgcolor="black")[0]
+            # if there are two values from the config file, then one of them defines
+            # the number of decimal places to display
+            if type(displaykeys[key]) == type([]):
+                displaytext = displaykeys[key][0]
+                display_decimals = int(displaykeys[key][1])
+                if key in fits_header:
+                    info_row += f"{displaytext}: {round(fits_header[key], display_decimals)} | "
+                elif key in primary_header:
+                    info_row += f"{displaytext}: {round(primary_header[key], display_decimals)} | "
+            else:
+                if key in fits_header:
+                    info_row += f"{displaykeys[key]}: {fits_header[key]} | "
+                elif key in primary_header:
+                    info_row += f"{displaykeys[key]}: {primary_header[key]} | "
+        file_info_text, file_info_pos = render_multiline_text(
+            info_row, screen_font)
 
         # draw information to the screen
         display.blit(filename_text, (0, 0))
-        display.blit(file_info, (0, SCREEN_SIZE[1]-FONT_SIZE))
+        display.blit(file_info_text, file_info_pos)
 
         # redraw the screen
         pygame.display.update()
@@ -158,29 +284,38 @@ def main(filenames, startindex, displaykeys, hduindex, displayhduname):
                     if event.key == pygame.K_RIGHT and current_image < len(filenames)-1:
                         current_image += 1
                         redraw = True
-                    # if the user presses space, then save the image to the output folder
+                    # if the user presses space, and the image is not currently set to be saved,
+                    # then save the image. Otherwise, remove the image from the list of images to process
                     if event.key == pygame.K_SPACE:
                         redraw = True
-                        saving_files.add(filenames[current_image])
-                    # if the user presses delete, then delete the image from the output folder
-                    if event.key == pygame.K_BACKSPACE:
-                        redraw = True
-                        # only delete the file if it already exists
-                        if (filenames[current_image] in saving_files):
+                        if filenames[current_image] in saving_files:
                             saving_files.remove(filenames[current_image])
+                        else:
+                            saving_files.add(filenames[current_image])
                     # if the user presses a number, then change the colormap
                     if event.key in [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9]:
                         new_scaling_idx = event.key - pygame.K_1
                         if new_scaling_idx < len(SCALING_FUNCS):
                             scaling_index = new_scaling_idx
                         redraw = True
+                    # if the user presses 'h', then open readme.md in a web browser
+                    if event.key == pygame.K_h:
+                        webbrowser.open(
+                            'https://github.com/yerkesobservatory/pipeline/blob/dev/utils/image_review/readme.md')
+
+                    # advance the colormap index
+                    if event.key == pygame.K_COMMA and colormap_index > 0:
+                        colormap_index -= 1
+                        redraw = True
+                    # retreat the colormap index
+                    if event.key == pygame.K_PERIOD and colormap_index < len(COLORMAP_NAMES) - 1:
+                        colormap_index += 1
+                        redraw = True
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Preview and filter FITS files')
-    parser.add_argument("--startindex", dest="startindex", metavar="O", type=int, nargs=1,
-                        help="The integer index of the start image (Default: 0)", required=False, default=0)
     parser.add_argument("--config", dest="config", metavar="O", type=str, nargs=1,
                         help="The name of the configuration file", required=False)
     parser.add_argument("file", metavar="F", type=str, nargs=argparse.REMAINDER,
@@ -191,41 +326,34 @@ if __name__ == "__main__":
 
     fits_files = args.file
     config_file = args.config
-    startindex = 0
     hduindex = 0
     displayhduname = False
     config_obj = None
-    
+    saving_piperun = False
+
     # if the configuration file exists, then use it to form a guess for the flag values
     # if values are passed in as arguments, then they override the values in the config file
     if (config_file != None):
         config_obj = configobj.ConfigObj(config_file[0])['image_filter']
-        print(config_obj)
-        if 'startindex' in config_obj:
-            startindex = int(config_obj['startindex'])
         if 'displaykeys' in config_obj:
             displaykeys = config_obj['displaykeys']
         if 'hduindex' in config_obj:
             hduindex = int(config_obj['hduindex'])
         if 'displayhduname' in config_obj:
-            displayhduname = bool(config_obj['displayhduname'])
-            
-    if type(args.startindex) == list:
-        startindex = args.startindex[0]
+            displayhduname = config_obj['displayhduname'] == "True"
 
-    saving_piperun = config_obj and 'save_piperun' in config_obj and bool(
-        config_obj['save_piperun'])
-
+    saving_piperun = config_obj and 'save_piperun' in config_obj and config_obj[
+        'save_piperun'] == "True"
     # if the user wants to save a piperun and the necessary flags are not present, then
     # do not even let them process images. This saves the time the user would have spent processing
     # the images only to find out they missed a single flag.
     if (saving_piperun and not('pythonpath' in config_obj and 'pipeconf' in config_obj and
-                                'pipemode' in config_obj and 'logfile' in config_obj and
-                                'loglevel' in config_obj and 'outputfolder' in config_obj)):
+                               'pipemode' in config_obj and 'logfile' in config_obj and
+                               'loglevel' in config_obj and 'outputfolder' in config_obj)):
         print("Missing flags to save a piperun!")
         print("Please ensure that your configuration file contains \'pythonpath\', \'pipeconf\', \'pipemode\', \'logfile\', \'loglevel\', and \'outputfolder\' entries")
         sys.exit()
-    
+
     elif fits_files == None or len(fits_files) == 0:
         print("No FITS files specified. Exiting...")
         parser.print_help()
@@ -233,10 +361,14 @@ if __name__ == "__main__":
 
     files_to_process = []
     files_to_process = main(
-        fits_files, startindex, displaykeys, hduindex, displayhduname)
+        fits_files, displaykeys, hduindex, displayhduname)
 
     # saves a piperun so the user can run 'darepyperun.py piperun_filtered' to continue processing their images
-    if 'save_piperun' in config_obj and bool(config_obj['save_piperun']):
+    if saving_piperun and len(files_to_process) > 0:
+        if 'piperun_name' in config_obj:
+            outfilename = config_obj['piperun_name']
+        else:
+            outfilename = "piperun_filtered.txt"
         piperun_out = f"pythonpath = {config_obj['pythonpath']}\n"
         config_files = config_obj['pipeconf'].replace(' ', '\n')
         piperun_out += f"pipeconf = {config_files}\n"
@@ -246,7 +378,6 @@ if __name__ == "__main__":
         piperun_out += f"outputfolder = {config_obj['outputfolder']}\n"
         files_out = '\n'.join(files_to_process)
         piperun_out += f"inputfiles = {files_out}\n"
-        with open(f"piperun_filtered.txt", 'w') as f:
+        with open(outfilename, 'w') as f:
             f.write(piperun_out)
-            print(piperun_out)
     sys.exit()
--- utils/image_review/readme.md
+++ utils/image_review/readme.md
@@ -1,5 +1,14 @@
 # Installing
-Just run `pip install -r requirements.txt` to install the necessary Python dependencies.
+The easiest way to download this program is to download it as part of the
+pipeline. If you are not using the pipeline, or find this tool useful for
+other purposes, then you can download it by clicking [this link](https://raw.githubusercontent.com/yerkesobservatory/pipeline/dev/utils/image_review/image_review.py) and
+then save the file to a folder on your computer. Make sure to also download
+the [requirements.txt](https://raw.githubusercontent.com/yerkesobservatory/pipeline/dev/utils/image_review/requirements.txt) file as well and save it to the same location.
+
+## Dependencies
+To install the necessary Python packages, `cd` to the folder where image_review
+is located and run `pip install -r requirements.txt` to install the necessary
+Python dependencies.
 
 # Running
 To run the image review script, run the following line in your terminal.
@@ -8,18 +17,32 @@ python3 image_review.py <input files>
 ```
 If you want to use more options,
 ```
-usage: image_review.py [-h] [--startindex O] [--config O] ...
+usage: image_review.py [-h] [--config O] ...
 
 Preview and filter FITS files
 
 positional arguments:
-  F               The FITS files to be loaded and previewed
+  F           The FITS files to be loaded and previewed
 
 optional arguments:
-  -h, --help      show this help message and exit
-  --startindex O  The integer index of the start image (Default: 0)
-  --config O      The name of the configuration file
+  -h, --help  show this help message and exit
+  --config O  The name of the configuration file
   ```
+
+# Keybindings
+<kbd>Esc</kbd>: Close the program and save a piperun if necessary
+
+<kbd>&#8592;</kbd>-<kbd>&#8594;</kbd>: Step through each image
+
+<kbd>0</kbd>-<kbd>9</kbd>: Switch between different color scales
+
+<kbd>Space</kbd>: Toggle between saving and not saving the image
+
+<kbd>h</kbd>: Opens this readme in a web browser
+
+<kbd>,</kbd>: Go back through the list of colormaps
+
+<kbd>.</kbd>: Go forward through the list of colormaps
 
 # Choosing Files
 Although it is possible to type out each of your fits files, I highly recommend learning to
@@ -31,51 +54,51 @@ Put another way, each fits file is named as follows:
 ```
 {object name}_{filter name}_{exposure time}_{binning}{HDR gain}_{date}_{time}_{username}_{telescope}_{number}_{pipestep}.fits
 ```
-If I wanted to select each RAW NGC2175 image, I could use `ngc2175*RAW.fits` as the filename. If I wanted
-each FCAL h-alpha image of M51, I would use `m51_h-alpha*FCAL.fits`. If I wanted each file that was taken
-on March 1st, 2022, I would use `*_220301_*.fits`. Each of these wildcards will search through the avilable
-fits files in the current folder and return a list of them to image_filter.py. If you wanted to search through
-other directories, you just need to append the directory name to the file: `/path/to/dir/*.fits`. You can also
-use wildcards in directories too: `/path/to/dir/*/m51*.fits` will take each image of M51 in each of the directories.
+If I wanted to select each RAW NGC2175 image, I could use `ngc2175*RAW.fits` as
+the filename. If I wanted each FCAL h-alpha image of M51, I would use
+`m51_h-alpha*FCAL.fits`. If I wanted each file that was taken on March 1st, 2022,
+I would use `*_220301_*.fits`. Each of these wildcards will search through the
+available fits files in the current folder and return a list of them to image_filter.py.
+If you wanted to search through other directories, you just need to append the directory
+name to the file: `/path/to/dir/*.fits`. You can also use wildcards in directories,
+too: `/path/to/dir/*/m51*.fits` will take each image of M51 in each of the directories.
 
 # Configuration Files and Saving Piperuns
-To streamline the process of running this script, it is possible to update settings in a configuration file. This
-avoids the need to type out long commands and speeds up the process of changing specific flags. Confiugrations files can be passed to the program using the `--config` flag.
+To streamline the process of running this script, it is possible to update
+settings in a configuration file. This avoids the need to type out long commands
+and speeds up the process of changing specific flags. Configurations files can be
+passed to the program using the `--config` flag.
 
-Configuration files are stored as headers in configuration files. If you are using deltaconfigs for your personal versions of the SEO pipeline, I recommend appending this `image_filter` section to your deltaconfig file. Otherwise, you can use a text file that only contains the following section.
+Configuration settings for image_review are stored as headers in configuration files. If you are using deltaconfigs in your personal versions of the SEO pipeline, I recommend appending a `image_filter` section to your deltaconfig file. Otherwise, you can use a text file that only contains an [image_filter] section.
 
 ## Saving piperuns
 Although it is possible to use this program to just review images, it is also possible to create a new piperun and use it to selectively process images. This can be useful if you want to stack images and want to avoid images that have lost tracking or images with poor seeing. In order to save a piperun, you MUST use a configuration file because the flags required to pass the piperun arguments would become unwieldy.
 
 ```
 [image_filter]
-    # If a file has multiple HDUs, this is the index of the HDU to load
-    hduindex = 0
+    # If a file has multiple HDUs, this is the index to load
+    hduindex = 1
 
-    # Decide whether to display the name of the HDU on screen
+    # Decide whether to display the name of the HDU
     displayhduname = True
 
-    # Out of the loaded FITS files, start with the following index
-    startindex = 0
+    # the name of the file to save the piperun to
+    piperun_name = "piperun_filtered.txt"
 
-    # Decide whether to save a new piperun that will allow you to further reduce images
-    # by running `darepyperun.py piperun_filtered.txt`
-    # These settings can be copied from a piperun you already use
+    # if save_piperun is True, and all the below variables are defined, then this program will output a new piperun
     save_piperun = True
     pythonpath = "/Users/ordelore/Documents/SEO/content/pipeline/source"
     pipeconf = "/Users/ordelore/Documents/SEO/content/pipeline/config/pipeconf_SEO.txt /Users/ordelore/Documents/SEO/content/pipeline/config/lorenzo_config.txt"
     pipemode = "filter_second_half"
     logfile = "/Users/ordelore/Documents/SEO/content/pipelog.txt"
     loglevel = "DEBUG"
-    # Make sure to change this line if you want to use a different output folder
-    # if you are reducing a different target.
-    outputfolder = "/Users/ordelore/Documents/SEO/content/data/m81"
+    outputfolder = "/Users/ordelore/Documents/SEO/content/data/m3"
 
     # to add more displayed fields, add them in this format
-    # header name = "display text"
-    # In the program, they will be displayed as
-    # "display text: {value of header name}"
+    # header name = "display text", decimal places
+    # In the program, they will be displayed as "display text: {value of header name truncated to the number of decimal places}"
     [[displaykeys]]
         DEWTEM1 = "Sensor Temp"
-        RHALF = "Half radius"
+        RHALF = "Half radius", 3
+        RHALFSTD = "Half radius std", 3
 ```
