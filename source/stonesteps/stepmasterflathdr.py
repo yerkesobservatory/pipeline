36d446388b5f466537a1a78439262acd121abc27
finianashmead <43968811+finianashmead@users.noreply.github.com>
2022-08-19 10:09:47 -0500

    Update stepmasterflathdr.py

--- source/stonesteps/stepmasterflathdr.py
+++ source/stonesteps/stepmasterflathdr.py
@@ -242,6 +242,8 @@ class StepMasterFlatHdr(StepLoadAux, StepMIParent):
         ## FILL THE ARRAYS WITHIN THESE LOOPS , LOOK AT TABLE TO MAKE SURE THEY'LL FILL IT CORRECTLY
         ## REPLACE ALL PRINTS WITH SELF.LOG.DEBUG
         for j in range(len(highgainlist)):
+            print(highgainlist[j].image)
+            print(highgainlist[j].image.shape)
             flatimage[0, j] = highgainlist[j].image
             # Calculate some statistical information.
             mad[j] = mad_std(image[j],ignore_nan=True)
