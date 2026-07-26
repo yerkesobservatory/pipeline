a7ca5ae237bcc403daaa87b2a58735a9b0c423ae
brazenly-incognito <ellagentry577@gmail.com>
2026-07-26 10:29:01 -0500

    Updated stepRGB
    Changed StepRGB image algorithm to include an exposure time check and log it in the pipelog

--- source/stonesteps/steprgb.py
+++ source/stonesteps/steprgb.py
@@ -115,51 +115,98 @@ class StepRGB(StepMOParent):
             filterorder_list = self.getarg('filterorder').split('|')
             filterprefs_list = self.getarg('filterprefs').split('|')
 
-            datain_filter_list = [element.getheadval('filter') for element in self.datain]
-            used_filter_flags = [False] * len(self.datain)
+            raw_data=[]
+            chosen_exptime = None
+            #def finding images()
+            #exp_times = [element.getheadval('EXPTIME') for element in self.datain]
+            #filters = [band.getheadval('filter') for band in self.datain]
+            #filters_times_chart = [exp_time_options, filters]
+
+            for element in self.datain:
+                exptime = float(element.getheadval('EXPTIME'))
+                filters = str(element.getheadval('filter'))
+                raw_data.append([element, filters, exptime])
+
+            used_input_flags = [False] * len(self.datain)
 
             if len(filterprefs_list) != 3:
                 self.log.error('Invalid number of preferred filters provided (should be 3): ' + 
                                self.getarg('filterprefs'))
             else:
-                # Locate data matching the filters specified in filterprefs
-                for i, preferred_filter in enumerate(filterprefs_list):
-                    for j, element in enumerate(self.datain):
-                        if element.getheadval('filter') == preferred_filter:
-                            datause[i] = element
-                            used_filter_flags[j] = True
-                            break
+                # sort raw data by decreasing exptime
+                sorted_raw_data=sorted(raw_data, key=lambda x: (x[2], x[1]),reverse=True)
+                print([row[2] for row in sorted_raw_data])
 
-            # Select missing filters from filterorder
-            filterorder_walker = 0
-            for i, channel in enumerate(datause):
-                if channel == None:
-                    for ordered_filter in filterorder_list[filterorder_walker:]:
-                        filterorder_walker = filterorder_walker + 1
-                        if ordered_filter in datain_filter_list:
-                            datain_index = datain_filter_list.index(ordered_filter)
-                            if not used_filter_flags[datain_index]:
-                                datause[i] = self.datain[datain_index]
-                                used_filter_flags[datain_index] = True
+                #group by exposure time
+                from collections import defaultdict
+                exptime_groups = defaultdict(list)
+                for row in sorted_raw_data:
+                    exptime_groups[row[2]].append(row)
+              
+                #search through groups for exptime with filterprefs
+                chosen_exptime=None
+                for exptime, rows in exptime_groups.items():
+                # get all filters available at this specific exposure time
+                    filters_at_this_time = {row[1] for row in rows}  
+                    # Check if filter_prefs are subset 
+                    if set(filterprefs_list).issubset(set(filters_at_this_time)):
+                        chosen_exptime = rows
+                        exptime_value = exptime
+                        print([row[0].filename for row in rows])
+                        break
+
+                #fill specific slots of datause in order of i,r,g
+                if chosen_exptime is not None:
+                    self.log.debug(f'Used {exptime_value}s exposure time.')
+                    for i, preferred_filter in enumerate(filterprefs_list):
+                        for row in chosen_exptime:
+                            element, filters, _ = row
+                            if filters==preferred_filter:
+                                datause[i]=element
+                                element_index=self.datain.index(element)
+                                used_input_flags[element_index]=True
                                 break
-                elif channel.getheadval('filter') in filterorder_list:
-                    filterorder_walker = filterorder_list.index(channel.getheadval('filter'))
+
+                # run original code if exposure times check fails
+                else:
+                    # Select missing filters from filterorder
+                    datain_filter_list = [element.getheadval('filter') for element in self.datain]
+                    filterorder_walker = 0
+                    for i, channel in enumerate(datause):
+                        if channel == None:
+                            for ordered_filter in filterorder_list[filterorder_walker:]:
+                                filterorder_walker = filterorder_walker + 1
+                                if ordered_filter in datain_filter_list:
+                                    datain_index = datain_filter_list.index(ordered_filter)
+                                    if not used_input_flags[datain_index]:
+                                        datause[i] = self.datain[datain_index]
+                                        used_input_flags[datain_index] = True
+                                        break
+                        elif channel.getheadval('filter') in filterorder_list:
+                            filterorder_walker = filterorder_list.index(channel.getheadval('filter'))
+                        
+                    # Select missing filters from any remaining fits files
+                    for i, channel in enumerate(datause):
+                        if channel == None:
+                            for j, datain_filter in enumerate(datain_filter_list):
+                                if not used_input_flags[j]:
+                                    datause[i] = self.datain[j]
+                                    used_input_flags[j] = True
+                                    break
+
+        self.log.debug('Files used: R = %s  G = %s  B = %s' % (datause[0].filename, datause[1].filename, datause[2].filename))
                     
-            # Select missing filters from any remaining fits files
-            for i, channel in enumerate(datause):
-                if channel == None:
-                    for j, datain_filter in enumerate(datain_filter_list):
-                        if not used_filter_flags[j]:
-                            datause[i] = self.datain[j]
-                            used_filter_flags[j] = True
-                            break
-                    
-        self.log.debug('Files used: R = %s  G = %s  B = %s' % (datause[0].filename, datause[1].filename, datause[2].filename) )
+        # Check exposure times
+        exposure_times = [element.getheadval('EXPTIME') for element in datause]
+        if len(set(exposure_times)) != 1:
+            self.log.warning(
+                'Input files have different exposure times: {exposure_times}'
+            )
+
         jpeg_dataout = DataFits(config = self.config)
         jpeg_dataout.header = datause[0].header
-	
-    # removing filter from filename   
-        # OLD: jpeg_dataout.filename = '_'.join(datause[0].filename.split('_')[:-9] + datause[0].filename.split('_')[-8:]) 
+
+        # removing filter from filename   
         filternames=self.config['addkeys']['filternames']
         filename = datause[0].filename
         for name in filternames:
@@ -170,7 +217,6 @@ class StepRGB(StepMOParent):
         if not filename:
             filename = "image"    
         jpeg_dataout.filename = filename
-
 
         img = datause[0].image
         img1 = datause[1].image
